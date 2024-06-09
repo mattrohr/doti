f516c12f925cad922a26fe9e5ee559c68294d7dc
Matt Rohr <67395122+mattrohr@users.noreply.github.com>
2024-06-09 18:56:31 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import telebot
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+
+bot = telebot.TeleBot("TOKEN")
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    # Load an image or create a new one
+    image = Image.new('RGB', (800, 600), color = (73, 109, 137))
+
+    # Initialize the drawing context
+    draw = ImageDraw.Draw(image)
+
+    # Load your custom font
+    #print(message.from_user.id)
+    if message.from_user.id == 6179535998:
+        font = ImageFont.truetype('MDWRohr/MDWRohr-Regular.ttf', size=50)
+    elif message.from_user.id == 65784765:
+        font = ImageFont.truetype('AMSoehartono/Amsoehartono-Regular.ttf', size=50)
+    else:
+        print(message.from_user.id)
+
+    # Position of the text
+    text_position = (20, 20)
+
+    # Text color
+    text_color = (255, 255, 255)
+
+    # Text content
+    text_content = message.text
+
+    # Wrap the text content
+    wrapped_text = '\n'.join(textwrap.wrap(text_content, width=18))
+
+    # Add wrapped text to your image
+    draw.text(text_position, wrapped_text, font=font, fill=text_color)
+
+    # Save the edited image
+    image.save('custom_font_image.png')
+    photo = open('custom_font_image.png', 'rb')
+    bot.send_photo(message.chat.id, photo)
+    # Delete the user's message
+    # you must promote users to admin in telegram app
+    bot.delete_message(message.chat.id, message.message_id)
+
+bot.infinity_polling()
